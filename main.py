e0e2764b2f2ffbcab0c1fc1d9bf697f78acc02a1
LucasVanHaaren <29121316+LucasVanHaaren@users.noreply.github.com>
2023-10-20 09:45:25 +0200

    add help menu

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 
 from sys import argv
 
+def show_help():
+	print(f"Usage : {argv[0]} <NAME>")
+	print("")
+	print("Print a generic greeting, or a personnal one if a name is provided")
+	print("")
+	print("ARGUMENTS:")
+	print("- NAME : (OPTIONNAL) Print a name to greet instead of 'Hello World'")
+	print("")
+
 if len(argv) == 1:
     print("Hello World !")
 elif len(argv) == 2:
     print(f"Hello {argv[1]}")
 else:
-    print("error")
+    show_help()
 
