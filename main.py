2bdea6113467646915bec6c0f80136ea62dae260
LucasVanHaaren <29121316+LucasVanHaaren@users.noreply.github.com>
2023-10-20 09:44:36 +0200

    add name option

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
-print("Hello World !")
+from sys import argv
+
+if len(argv) == 1:
+    print("Hello World !")
+elif len(argv) == 2:
+    print(f"Hello {argv[1]}")
+else:
+    print("error")
 
